ab8741ed09ff8d113487c75493231331bc5ade95
Arman hossain <86116688+Arman3875@users.noreply.github.com>
2024-11-09 07:21:04 +0000

    init

--- Numpy/29.py
+++ Numpy/29.py
@@ -2,10 +2,10 @@
 import numpy as np
 
 # Creating arrays for conditions
-array = np.array([0, 1, 2, 3])
-choices = [array*2, array+10, array**2]
+array = np.array([2, 1, 6, 3])
+choices = [list(array*2), list(array+10), list(array**2)]
 
 # Using np.choose to apply different conditions
-result = np.choose(array, choices)
+result = np.choose([2,1,0,1], choices)
 
 print('Result using np.choose:', result)
--- Numpy/30.py
+++ Numpy/30.py
@@ -5,6 +5,6 @@ import numpy as np
 array_2d = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
 
 # Combining boolean, fancy, and slice indexing
-result = array_2d[array_2d > 30][:, [0, 2]]
+result = array_2d[array_2d > 30][:2]
 
 print('Combined Indexing Result:', result)
